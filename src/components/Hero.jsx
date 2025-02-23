2902f75e73b99d29df57bd852b2230f80e3f28f8
DarthKenar - 💻 <federico.vega2222@gmail.com>
2025-02-23 17:22:15 -0300

    style: color & images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <h3 className="text-secondaryColor text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
-            Descubre nuevas herramientas analíticas
+          <h3 className="text-secondaryColor text-sm sm:text-base  mb-6 sm:mt-32 mt-16 font-bold">
+            Digitaliza tu empresa, centraliza tus datos y toma decisiones.
           </h3>
         </motion.div>
         <motion.div
@@ -28,11 +28,10 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-8 md:px-20 lg:px-4">
-            <h1 className="inline md:hidden">Herramientas Analíticas</h1>
-            <h1 className="hidden md:inline">Herramientas Analíticas</h1>
+            <h1>Soluciones digitales</h1>
           </div>
           <h1 className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-20 md:px-24 lg:px-24">
-            para desarrolladores
+          para tu empresa.
           </h1>
         </motion.div>
         <motion.div
@@ -41,7 +40,7 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <h2 className="text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-            Experimenta integraciones comerciales sin problemas y potentes ideas con nuestras herramientas analíticas de vanguardia.
+            Webs a medida, sistemas de gestión y analíticas avanzadas.
           </h2>
         </motion.div>
         <motion.div
