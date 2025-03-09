e1d6c3ca3f0c6abb3b122b3ef300320f13077c5f
DarthKenar - 💻 <federico.vega2222@gmail.com>
2025-03-09 01:19:23 -0300

    style:wspbtn

--- src/components/shared/wsp.jsx
+++ src/components/shared/wsp.jsx
@@ -5,7 +5,7 @@ const WspButton = () => {
   return (
     <a
       // className="flex justify-center items-center bg-green-600 hover:bg-green-800 pt-2 pr-8 pb-2 pl-6 main-border-gray rounded-xl text-white text-sm hover:scale-110 transition-all duration-300 shine-effect"
-      className="flex justify-center items-center bg-bgDark2 hover:bg-bgDark3 border border-primaryColor border-solid rounded-xl w-64 sm:w-52 h-12 font-medium text-primaryText transition cursor-pointer shine-effect"
+      className="flex justify-center items-center bg-bgDark2 hover:bg-bgDark3 border border-solid rounded-xl w-64 sm:w-52 h-12 font-medium text-primaryText transition cursor-pointer shine-effect"
       href="https://wa.me/+5492613337751?text=Te escribo desde `ioseph.dev`. Mi nombre es "
       target="_blank"
       aria-label="numero telefónico"
